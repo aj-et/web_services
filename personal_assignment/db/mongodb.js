4153c0fa4c3df9551727300087373b866b571233
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-09-20 21:03:50 -0600

    added .rest file

--- personal_assignment/db/mongodb.js
+++ personal_assignment/db/mongodb.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const uri = process.env.MONGODB_URI;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri);
 
 const run = async () => {
     try {
--- personal_assignment/server.js
+++ personal_assignment/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongodb = require('./db/mongodb');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json()); // This will handle JSON requests
 
