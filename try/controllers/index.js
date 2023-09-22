b1909be60fa70b98f97bd14c4e8bc27d95dd549b
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-09-22 11:01:27 -0600

    removed unneccesary stuff

--- try/controllers/index.js
+++
@@ -1,5 +0,0 @@
-const getName = (req, res) => {
-    res.json({ name: 'Naomi Tumbokon' });
-};
-
-module.exports = { getName };
--- try/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-
-const baseController = require('../controllers/index');
-
-routes.get('/', baseController.getName);
-
-module.exports = routes;
