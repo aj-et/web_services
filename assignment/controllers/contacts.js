e3ff581c2c00e2cd7af5a4d21caf5f2292c6fc0f
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-09-27 22:21:44 -0600

    Added CREATE, PUT, DELETE

--- assignment/controllers/contacts.js
+++ assignment/controllers/contacts.js
@@ -1,9 +1,9 @@
 const { client, ObjectId } = require('../db/mongodb');
 
-const getAll = async (req, res) => {
-    const db = client.db('people'); // Database name
-  const collection = db.collection('contacts'); // Collection name
+const db = client.db('people'); // Database name
+const collection = db.collection('contacts'); // Collection name
 
+const getAll = async (req, res) => {
     try {
         const contacts = await collection.find({}).toArray();
         res.json(contacts);
@@ -14,9 +14,6 @@ const getAll = async (req, res) => {
 }
 
 const getSingle = async (req, res) => {
-    const db = client.db('people'); // Database name
-    const collection = db.collection('contacts'); // Collection name
-
     const contactId = req.params.id;
 
     try {
@@ -26,10 +23,63 @@ const getSingle = async (req, res) => {
         } else {
             res.status(404).send('Contact not found');
         }
-        } catch (error) {
+    } catch (error) {
         console.error('Error retrieving contact:', error);
         res.status(500).send('Internal Server Error');
         }
 }
 
-module.exports = { getAll, getSingle }
+const createContact = async (req, res) => {
+    const newContact = req.body;
+
+    try {
+        const result = await collection.insertOne(newContact);
+        if (result) {
+            res.status(201).json({ message: 'Contact created successfully' });
+        } else {
+            res.status(500).send('Error creating contact');
+        }
+    } catch (error) {
+        console.error('Error creating contact:', error);
+        res.status(500).json({ message: 'Error creating contact', error: error.message });
+    }
+}
+
+const updateContact = async (req, res) => {
+    const contactId = req.params.id; // Assuming the contact ID is passed in the URL
+    const updatedContact = req.body;
+
+    try {
+        const result = await collection.updateOne(
+            { _id: new ObjectId(contactId) },
+            { $set: updatedContact }
+        );
+
+        if (result.modifiedCount === 1) {
+            res.status(204).json({ message: 'Contact updated successfully' });
+        } else {
+            res.status(404).send('Contact not found');
+        }
+    } catch (error) {
+        console.error('Error updating contact:', error);
+        res.status(500).json({ message: 'Internal Server Error', error: error.message });
+    }
+}
+
+const deleteContact = async (req, res) => {
+    const contactId = req.params.id; // Assuming the contact ID is passed in the URL
+
+    try {
+        const result = await collection.deleteOne({ _id: new ObjectId(contactId) });
+        if (result) {
+            res.status(200).json({ message: 'Contact deleted successfully' });
+        } else {
+            res.status(404).send('Contact not found');
+        }
+    } catch (error) {
+        console.error('Error deleting contact:', error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact }
