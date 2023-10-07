edd75ea98b1ae235ec47707fc69654b174b5a12a
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-10-07 13:48:08 -0600

    changed http to https beacuse of network failure in render

--- assignment/swagger.js
+++ assignment/swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: 'Contacts API',
     },
     host: 'web-service-86g0.onrender.com',
-    schemes: ['http'],
+    schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
