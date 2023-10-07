bc7db07904e5077e968afdb245c03d9c504b4038
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-10-07 13:21:26 -0600

    added cors

--- assignment/server.js
+++ assignment/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const mongodb = require('./db/mongodb');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json()); // This will handle JSON requests
 
 app.use('/', require('./routes/index'))
